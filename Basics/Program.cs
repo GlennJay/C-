501fd2bef6f7624aafca1876cb23ee758748f630
GlennJay <glennwjason@gmail.com>
2020-02-24 12:34:47 -0500

    working on classes

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -8,27 +8,53 @@ using System.Threading.Tasks;
 
 namespace Basics
 {
+    //example class for later
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce(){
+            Console.WriteLine("Hello my name is " + firstName + lastName);
+        }
+
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            string characterName = "John"; //string variable
+
+
+            //SECTION basic
+
+            /* string characterName = "John"; //string variable
             int characterAge = 35; //int variable
             char characterInitial = 'J'; //character variable only one character
-            bool isMale = true;
+            bool isMale = true; */
 
             /* decimal point numbers
              * float least accurate
              * double next accurate
              * decimal most accurate
              */
-            double characterWishedAge = 21.0;
+            /* double characterWishedAge = 21.0;
 
             Console.WriteLine("There once was a man named " + characterName);
             Console.WriteLine("He was " + characterAge + " years old ");
             Console.WriteLine("He really Liked his inital being " + characterInitial);
             Console.WriteLine("But didn't like being " + characterAge);
-            Console.WriteLine("He wished he could be " + characterWishedAge);
+            Console.WriteLine("He wished he could be " + characterWishedAge); */
+
+            //SECTION 4 non primitve types
+            //classes
+            var john = new Person();
+            john.firstName = "John";
+            john.lastName = "Smith";
+            john.Introduce();
+
 
             Console.ReadLine(); //stops window from exiting
         }
