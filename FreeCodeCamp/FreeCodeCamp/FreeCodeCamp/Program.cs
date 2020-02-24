be4df5b0fea6536cd36ce21e5b39faaa036f7150
GlennJay <glennwjason@gmail.com>
2020-02-24 16:42:15 -0500

    stopped at 58:26

--- FreeCodeCamp/FreeCodeCamp/FreeCodeCamp/Program.cs
+++ FreeCodeCamp/FreeCodeCamp/FreeCodeCamp/Program.cs
@@ -10,6 +10,26 @@ namespace FreeCodeCamp
     {
         static void Main(string[] args)
         {
+            //STRINGS
+            /*string phrase = "This world can be so cold!";
+
+            Console.WriteLine("the length of the string is " + phrase.Length);
+            Console.WriteLine("all upper case " + phrase.ToUpper() + " Now lower case " + phrase.ToLower());
+            Console.WriteLine("phrase contains world " + phrase.Contains("world"));
+            Console.WriteLine(phrase.IndexOf("be"));
+            Console.WriteLine(phrase.Substring(11, 3));*/
+
+            //NUMBERS
+            
+            Console.WriteLine("adding a int with a decimal will give a decimal");
+            Console.WriteLine("60.5 + 67 = " + (60.5 + 67));
+            Console.WriteLine(Math.Abs(-58));
+            Console.WriteLine(Math.Pow(3, 2));
+
+            //left off at 58:26
+
+            Console.ReadLine();
+
         }
     }
 }
