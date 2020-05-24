d19b1e3f7c5a3230f26d15c8a8fab33fa951adba
GlennJay <glennwjason@gmail.com>
2020-05-24 11:45:05 -0400

    started practice

---
+++ Excersices/Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seven = 7;
+            double twentyfiveandhalf = 25.5;
+            int five = 5;
+            double sixpointsix = 6.6;
+            int one = 1;
+            int sum = 5;
+
+            if (sum == five)
+            {
+                Console.WriteLine("the value sum is equal to " + five );
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
