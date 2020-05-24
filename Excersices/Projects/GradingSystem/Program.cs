f457dd2f018856f4822e8fd797e8f2da33509668
GlennJay <glennwjason@gmail.com>
2020-05-24 12:14:10 -0400

    Projects folder

---
+++ Excersices/Projects/GradingSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*The start of the grading program
+ */
+
+
+namespace GradingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
