70afe31b16b5eb550ee3a8c26e79ff1a5857615e
GlennJay <glennwjason@gmail.com>
2020-05-26 12:10:19 -0400

    fix some errors

--- Excersices/GradingSystem/Program.cs
+++ Excersices/GradingSystem/Program.cs
@@ -9,7 +9,7 @@ namespace GradingSystem
 {
     class Program
     {
-        static int numberOfStudents = 10;
+        static int numberOfStudents = 3;
         static Student[] students = new Student[numberOfStudents];
         static String[] courses = new string[]{"Science", "Math", "History", "English"};
 
@@ -27,6 +27,10 @@ namespace GradingSystem
             Student student1 = new Student( john, nine);
             Student student2 = new Student(mary, ten);
             Student student3 = new Student(jayson, eleven);
+            students[0] = student1;
+            students[1] = student2;
+            students[2] = student3;
+           
 
             ShowMainMenu();
            
@@ -56,9 +60,9 @@ namespace GradingSystem
                     Console.WriteLine("What is the name of the student you want to enter?");
                      student.name = Console.ReadLine();
 
-                    Console.WriteLine("What is the grade that {0} is in?", name);
-                     student.gradeLevel = Console.ReadLine();
-                    students.Add(student);
+                    Console.WriteLine("What is the grade that {0} is in?", student.name);
+                     student.gradeLevel = Convert.ToInt32(Console.ReadLine());
+                    students[0] = student;
                     break;
                     //option to enter grades for specific student
                 case 2:
@@ -69,9 +73,9 @@ namespace GradingSystem
                     String course = Console.ReadLine();
 
                     Student FoundStudent = FindStudent(name);
-                    FindCourse(course,name);
+                    FindCourse(course,FoundStudent);
                     break;
-                case default:
+                 default:
                     Console.WriteLine("Not a valid option");
                     break;
             
@@ -81,15 +85,17 @@ namespace GradingSystem
 
         public static Student FindStudent(string name)
         {
+            Student foundStudent = new Student();
             bool found = false;
             while(found == false)
             {
                 for(int i = 0; i < students.Length; i++)
                 {
-                    if(students[i] == name)
+                    if(students[i].name == name)
                     {
+                        
+                        foundStudent = students[i];
                         found = true;
-                        return  students[i];
                         
                     }
                 }
@@ -101,11 +107,12 @@ namespace GradingSystem
                     name = Console.ReadLine();
                 }
             }
+            return foundStudent;
             
             
         }
 
-        public static String FindCourse(String course, Student name)
+        public static void FindCourse(String course, Student name)
         {
             course = course.ToLower();
             bool foundCourse = false;
@@ -129,18 +136,19 @@ namespace GradingSystem
                         foundCourse = true;
                         break;
 
-                    case "history":
-                        name.setHistoryGrades();
+                    case "english":
+                        name.setEnglishGrades();
                         foundCourse = true;
                         break;
 
                     default:
                         Console.WriteLine("Course does not exist");
+                        Console.WriteLine("Enter the correct course: ");
+                        course = Console.ReadLine();
                         break;
 
                 }
-                Console.WriteLine("Enter the correct course: ");
-                course = Console.ReadLine();
+                
             }
 
         }
